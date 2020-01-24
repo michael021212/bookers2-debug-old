18433360cf2ffb46146f54ca850b6769bc322200
tanakashun <michael021212@gmail.com>
2020-01-24 08:30:11 +0000

    finish

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!
+	before_action :baria_user, only: [:update]
+	before_action :set_user, only: [:show, :index, :edit]
+  before_action :ensure_correct_user,only: [:edit]
+
+  def show
+  	@user = User.find(params[:id])
+  	@books = @user.books
+  	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+  end
+
+  def index
+  	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
+  	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+	end
+
+  def edit
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to user_path(@user), notice: "successfully updated user!"
+		else
+			@book = Book.new
+			@books = @user.books
+			flash.now[:notice] = 'error'
+  		render "show"
+  	end
+  end
+
+	private
+
+	def set_user
+		@user = current_user
+	end
+
+  def user_params
+  	params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  #url直接防止　メソッドを自己定義してbefore_actionで発動。
+   def baria_user
+  	unless params[:id].to_i == current_user.id
+  		redirect_to user_path(current_user)
+  	end
+	 end
+
+	 def ensure_correct_user
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+
+end
